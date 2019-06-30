6d0ecf23e4fa48eb10fc183eea32e85792208722
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-06-30 09:16:07 +0900

    Update XCubic

--- src/com/example/itp1/one/XCubic.kt
+++ src/com/example/itp1/one/XCubic.kt
@@ -5,7 +5,7 @@ import java.util.*
 fun main(args: Array<String>) {
     val sc = Scanner(System.`in`)
     try {
-        val x = Integer.parseInt(sc.nextLine())
+        val x = sc.nextInt()
         println(x * x * x)
     } catch (e: Exception) {
         println(e)
