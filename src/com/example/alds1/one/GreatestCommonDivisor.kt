6b7c0c2e4d724ffebd84baad365bb8065f087caa
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-08-09 07:26:28 +0900

    Update Greatest Common Divisor

--- src/com/example/alds1/one/GreatestCommonDivisor.kt
+++ src/com/example/alds1/one/GreatestCommonDivisor.kt
@@ -1,5 +1,6 @@
 package com.example.alds1.one
 
+import java.lang.Integer.max
 import java.lang.Integer.min
 import java.util.Scanner
 
@@ -12,12 +13,21 @@ fun main(args: Array<String>) =
                     || a > 1_000_000_000 || b > 1_000_000_000) {
                 throw Exception("Illegal inputs.")
             }
-            println(gcd(a, b))
+            println(fastGcd(a, b))
         } catch (e: Exception) {
             println(e)
         }
 
-fun gcd(a: Int, b: Int): Int {
+fun fastGcd(a: Int, b: Int): Int {
+    val x = max(a, b)
+    val y = min(a, b)
+    if ((x % y) == 0) {
+        return y
+    }
+    return fastGcd(y, (x % y))
+}
+
+fun slowGcd(a: Int, b: Int): Int {
     val n = min(a, b)
     for (i in n downTo 1) {
         if (a % i == 0 && b % i == 0) {
