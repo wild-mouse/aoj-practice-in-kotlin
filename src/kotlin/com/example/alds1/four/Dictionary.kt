c5f20d0740024aecac37448c9c86170a1dd321cc
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-08-11 13:27:57 +0900

    Add Dictionary

---
+++ src/kotlin/com/example/alds1/four/Dictionary.kt
@@ -0,0 +1,33 @@
+package com.example.alds1.four
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    try {
+        val sc = Scanner(System.`in`)
+        val n = Integer.parseInt(sc.nextLine())
+        val d = Dictionary()
+        for (i in 0 until n) {
+            val c_s = sc.nextLine().split(" ")
+            if (c_s[0] == "insert") {
+                d.insert(c_s[1])
+            }
+            if (c_s[0] == "find") {
+                println(d.find(c_s[1]))
+            }
+        }
+    } catch (e: Exception) {
+        println(e)
+    }
+}
+
+class Dictionary{
+    private val a = mutableListOf<String>()
+
+    fun insert(s: String) {
+        if (a.contains(s)) return
+        a.add(s)
+    }
+
+    fun find(s: String) = if (a.contains(s)) "yes" else "no"
+}
